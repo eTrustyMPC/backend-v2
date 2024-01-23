2d0c7524bbb4cfad85366b2634bd1591244ec265
Ilia Smirnov <php1602agregator@gmail.com>
2024-01-23 09:29:52 +0300

    feat(empty): set up oauth providers config

--- src/application.ts
+++ src/application.ts
@@ -3,15 +3,19 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import { AuthenticationComponent } from '@loopback/authentication';
-import { BootMixin } from '@loopback/boot';
-import { ApplicationConfig } from '@loopback/core';
-import { RepositoryMixin } from '@loopback/repository';
-import { RestApplication } from '@loopback/rest';
-import { CrudRestComponent } from '@loopback/rest-crud';
-import { ServiceMixin } from '@loopback/service-proxy';
-import { MySequence } from './sequence';
-import { config as loadEnv } from "dotenv";
+import {AuthenticationComponent} from '@loopback/authentication';
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {CrudRestComponent} from '@loopback/rest-crud';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent
+} from '@loopback/rest-explorer';
+import {ServiceMixin} from '@loopback/service-proxy';
+import {config as loadEnv} from "dotenv";
+import {MySequence} from './sequence';
 loadEnv();
 
 export class OAuth2LoginApplication extends BootMixin(
@@ -24,7 +28,14 @@ export class OAuth2LoginApplication extends BootMixin(
     this.sequence(MySequence);
 
     this.component(AuthenticationComponent);
+
+    // REST
+    this.component(RestExplorerComponent);
     this.component(CrudRestComponent);
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
 
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
